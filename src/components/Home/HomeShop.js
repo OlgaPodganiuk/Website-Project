0ff9f3bd1770cb1780c02ab5c117a239d59263ee
OlgaPodganiuk <olga79666@gmail.com>
2022-01-10 14:44:30 +0200

    img

--- src/components/Home/HomeShop.js
+++ src/components/Home/HomeShop.js
@@ -1,7 +1,7 @@
 import react from 'react';
-import img1 from '/img/1.jpg';
-import img2 from '/img/2.jpg';
-import img3 from '/img/3.jpg';
+import img1 from '../img/1.jpg';
+import img2 from '../img/2.jpg';
+import img3 from '../img/3.jpg';
 
 import './HomeShop.css';
 
